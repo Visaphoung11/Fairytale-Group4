4b4117311d1e0ae4a5f36c23d0075924d9fc0836
viaslphoung <visalphoung22@gmail.com>
2025-04-02 19:49:20 +0700

    storycard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/navbar.tsx";
 import AdventureBanner from "./pages/story.tsx"; // Ensure this component exists
+import StoryGrid from "./components/StoryGrid.tsx";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <AdventureBanner />
+      {/* <div className="p-6 bg-gray-100 min-h-screen"> */}
+      <StoryGrid />
+      {/* </div> */}
     </>
   );
 };
